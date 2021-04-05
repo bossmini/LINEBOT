6502251146fa67996e33b9aa4438511f91509a4d
bossmini <46773641+bossmini@users.noreply.github.com>
2021-04-05 11:01:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Reply with two static messages
+// Echo reply
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -9,24 +9,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
     res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token) {
+function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {9qyQGZVscJyaG/EbtENRfaSvXRBP4lJ+4Q7YAFv7pS1Wx7sOtzi+LDUJdax0aD84NeuWrZ7HOEaLu8pj6w1JoD1K419iEApXx8YgJa2iHYb1PPTWyLWE4CBKYBfdb78zr+Oh3VBrG1RJgdJOWi5AnQdB04t89/1O/w1cDnyilFU=}'
-     }
+    }
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
+            text: msg
         }]
     })
     request.post({
