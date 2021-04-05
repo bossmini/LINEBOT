8f288d5bab466d967b494184dbc92a5732057848
bossmini <46773641+bossmini@users.noreply.github.com>
2021-04-05 10:49:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -18,15 +18,11 @@ function reply(reply_token) {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {9qyQGZVscJyaG/EbtENRfaSvXRBP4lJ+4Q7YAFv7pS1Wx7sOtzi+LDUJdax0aD84NeuWrZ7HOEaLu8pj6w1JoD1K419iEApXx8YgJa2iHYb1PPTWyLWE4CBKYBfdb78zr+Oh3VBrG1RJgdJOWi5AnQdB04t89/1O/w1cDnyilFU=}'
     }
-    let body = JSON.stringify({
+      let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
+            text: msg
         }]
     })
     request.post({
